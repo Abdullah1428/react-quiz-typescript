aee21e1088245324c1f917b875b47e33d66009be
Abdullah1428 <abdullahcse1428@gmail.com>
2021-08-25 19:03:19 +0200

    added basic app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
-import logo from './logo.svg'
 
-function App() {
-  return <div className='App'>Quiz</div>
+// components
+import QuestionCard from './components/QuestionCard'
+
+const App = () => {
+  const startTrivia = async () => {}
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
+
+  const nextQuestion = () => {}
+
+  return (
+    <div className='App'>
+      <h1>REACT QUIZ</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'>Score: </p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className='next' onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QuestionCard = () => {
+  return (
+    <div>
+      <h1>Question Card</h1>
+    </div>
+  )
+}
+
+export default QuestionCard
